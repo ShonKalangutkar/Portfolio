5c7e84eab2b723b1d8b3bcc6062478b2d23d3371
ShonKalangutkar <shonkalangutkar1998@gmail.com>
2026-03-02 11:16:09 +0530

    named changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from "./sections/Navbar.jsx";
-import Hero from "./sections/Hero.jsx";
-import About from "./sections/About.jsx"
-import Projects from "./sections/Projects.jsx"
-import Clients from "./sections/Clients.jsx"
-import Contact from "./sections/Contact.jsx"
-import Footer from "./sections/Footer.jsx"
-import Experience from "./sections/Experience.jsx"
+import Navbar from "./sections/Navbar.tsx";
+import Hero from "./sections/Hero.tsx";
+import About from "./sections/About.tsx"
+import Projects from "./sections/Projects.tsx"
+import Clients from "./sections/Clients.tsx"
+import Contact from "./sections/Contact.tsx"
+import Footer from "./sections/Footer.tsx"
+import Experience from "./sections/Experience.tsx"
 
 const App = () => {
   return (
